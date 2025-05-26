b66c9a51b89154275eb3d85885b2ecf72c6c2206
wryuin <newexx@list.ru>
2025-05-26 09:42:01 +0500

    Update MetricsManager.java

--- src/main/java/me/wryuin/metrics/MetricsManager.java
+++ src/main/java/me/wryuin/metrics/MetricsManager.java
@@ -39,7 +39,7 @@ public class MetricsManager {
         
         // bStats metrics
         // TODO #1: Add plugin ID from bStats
-        Metrics metrics = new Metrics(plugin, 12345);
+        Metrics metrics = new Metrics(plugin, 25983);
         metrics.addCustomChart(new SingleLineChart("transactions", () -> 
             (int) transactionsCounter.count()));
         metrics.addCustomChart(new SimplePie("storage_type", () -> 
